09d256aa4bf748c523db9ac2ce8847ae2395acb7
Sujan Bhatta <165800292+sujan0629@users.noreply.github.com>
2024-12-02 08:48:53 +0545

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev E-Commerce Application",
-  description: "A complete e-commerce application with Next.js and Wix",
+  title: "E-Commerce Application for Portraits",
+  description: "A complete e-commerce application",
 };
 
 export default function RootLayout({
